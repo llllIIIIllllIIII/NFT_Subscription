b6af910050e21c8609526922edc65c8970c52dcb
張哲瑋 <henrychang@zhangzheweidebijixingdiannao.local>
2025-04-05 21:49:42 +0800

    remove ERC721Enumerable

--- src/SubscriptionNFT.sol
+++ src/SubscriptionNFT.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract SubscriptionNFT is ERC721Enumerable, Ownable(msg.sender) {
+contract SubscriptionNFT is ERC721, ERC721Burnable, Ownable {
     struct SubscriptionInfo {
+        uint256 startTime;
         uint256 expiration;
         uint256 price;
     }
@@ -16,6 +18,8 @@ contract SubscriptionNFT is ERC721Enumerable, Ownable(msg.sender) {
     uint256 public duration;
     uint256 public price;
     address public provider;
+
+    uint256 private _currentTokenId;
 
     event Subscribed(address indexed subscriber, uint256 tokenId, uint256 expiration);
     event SubscriptionUpdated(uint256 newDuration, uint256 newPrice);
@@ -27,7 +31,7 @@ contract SubscriptionNFT is ERC721Enumerable, Ownable(msg.sender) {
         uint256 _duration,
         uint256 _price,
         address[] memory _paymentTokens
-    ) ERC721(name, symbol) {
+    ) ERC721(name, symbol) Ownable(msg.sender) {
         provider = _provider;
         duration = _duration;
         price = _price;
@@ -36,13 +40,13 @@ contract SubscriptionNFT is ERC721Enumerable, Ownable(msg.sender) {
 
     function subscribe(address paymentToken) external payable {
         require(isValidPaymentToken(paymentToken), "Invalid payment token");
-        uint256 tokenId = totalSupply() + 1;
 
-        // Transfer payment
+        uint256 tokenId = ++_currentTokenId;
+
         require(IERC20(paymentToken).transferFrom(msg.sender, address(this), msg.value), "Payment failed");
 
         uint256 expiration = block.timestamp + duration;
-        subscriptions[tokenId] = SubscriptionInfo(expiration, msg.value);
+        subscriptions[tokenId] = SubscriptionInfo(block.timestamp, expiration, msg.value);
         _mint(msg.sender, tokenId);
 
         emit Subscribed(msg.sender, tokenId, expiration);
@@ -50,6 +54,7 @@ contract SubscriptionNFT is ERC721Enumerable, Ownable(msg.sender) {
 
     function updateSubscription(uint256 newDuration, uint256 newPrice) external onlyOwner {
         duration = newDuration;
+        price = newPrice;
         emit SubscriptionUpdated(newDuration, newPrice);
     }
 
@@ -62,14 +67,25 @@ contract SubscriptionNFT is ERC721Enumerable, Ownable(msg.sender) {
         return false;
     }
 
+    function isExpired(uint256 tokenId) external view returns (bool) {
+        return block.timestamp >= subscriptions[tokenId].expiration;
+    }
+
     function burnExpired(uint256 tokenId) external {
         require(block.timestamp >= subscriptions[tokenId].expiration, "Subscription not expired");
+
         address owner = ownerOf(tokenId);
+        require(owner != address(0), "Invalid owner");
+
         _burn(tokenId);
 
-        // Release funds to provider
         uint256 amount = subscriptions[tokenId].price;
         delete subscriptions[tokenId];
+
         payable(provider).transfer(amount);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _currentTokenId;
     }
 }
