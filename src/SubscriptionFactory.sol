2db013830d83a4eddac3ec2b5096e0c69aeb06be
張哲瑋 <henrychang@zhangzheweidebijixingdiannao.local>
2025-04-06 01:43:44 +0800

    update factory contract

--- src/SubscriptionFactory.sol
+++ src/SubscriptionFactory.sol
@@ -6,7 +6,10 @@ import "./SubscriptionNFT.sol";
 contract SubscriptionFactory {
     address[] public subscriptionContracts;
 
-    event SubscriptionCreated(address indexed provider, address contractAddress);
+    mapping(string => address) public nameToContract;
+    mapping(address => string) public contractToName;
+
+    event SubscriptionCreated(address indexed provider, address contractAddress, string name);
 
     function createSubscription(
         string memory name,
@@ -15,6 +18,8 @@ contract SubscriptionFactory {
         uint256 price,
         address[] memory paymentTokens
     ) external {
+        require(nameToContract[name] == address(0), "Name already used");
+
         SubscriptionNFT newSubscription = new SubscriptionNFT(
             name,
             symbol,
@@ -23,11 +28,29 @@ contract SubscriptionFactory {
             price,
             paymentTokens
         );
-        subscriptionContracts.push(address(newSubscription));
-        emit SubscriptionCreated(msg.sender, address(newSubscription));
+
+        address contractAddress = address(newSubscription);
+
+        subscriptionContracts.push(contractAddress);
+        nameToContract[name] = contractAddress;
+        contractToName[contractAddress] = name;
+
+        emit SubscriptionCreated(msg.sender, contractAddress, name);
     }
 
     function getAllSubscriptions() external view returns (address[] memory) {
         return subscriptionContracts;
+    }
+
+    function getSubscriptionCount() external view returns (uint256) {
+        return subscriptionContracts.length;
+    }
+
+    function getContractByName(string memory name) external view returns (address) {
+        return nameToContract[name];
+    }
+
+    function getNameByContract(address contractAddress) external view returns (string memory) {
+        return contractToName[contractAddress];
     }
 }
